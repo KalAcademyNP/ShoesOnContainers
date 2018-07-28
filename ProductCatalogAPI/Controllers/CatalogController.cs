42c464706caf5b03d58dcc247eca7a9ef8bd8556
Kal Academy <info@kalacademy.org>
2018-07-28 16:55:21 -0700

    Catalog controller actions

---
+++ ProductCatalogAPI/Controllers/CatalogController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ProductCatalogAPI.Data;
+using ProductCatalogAPI.Domain;
+
+namespace ProductCatalogAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Catalog")]
+    public class CatalogController : Controller
+    {
+        private readonly CatalogContext _catalogContext;
+
+        public CatalogController(CatalogContext catalogContext)
+        {
+            _catalogContext = catalogContext;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> CatalogTypes()
+        {
+            var items = await _catalogContext.CatalogTypes.ToListAsync();
+            return Ok(items);
+
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> CatalogBrands()
+        {
+            var items = await _catalogContext.CatalogBrands.ToListAsync();
+            return Ok(items);
+        }
+
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> Items(
+            [FromQuery] int pageSize = 6, 
+            [FromQuery] int pageIndex = 0)
+        {
+            var totalItems = await _catalogContext.CatalogItems
+                              .LongCountAsync();
+            var itemsOnPage = await _catalogContext.CatalogItems
+                              .OrderBy(c => c.Name)
+                              .Skip(pageSize * pageIndex)
+                              .Take(pageSize)
+                              .ToListAsync();
+            itemsOnPage = ChangeUrlPlaceHolder(itemsOnPage);
+            return Ok(itemsOnPage);
+        }
+
+        private List<CatalogItem> ChangeUrlPlaceHolder(List<CatalogItem> items)
+        {
+            items.ForEach(
+                x => x.PictureUrl = 
+                x.PictureUrl
+                .Replace("http://externalcatalogbaseurltobereplaced",
+                "http://localhost:6000"));
+            return items;
+        }
+
+    }
+}
---
+++ ProductCatalogAPI/Controllers/PicController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductCatalogAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Pic")]
+    public class PicController : Controller
+    {
+        private readonly IHostingEnvironment _env;
+        public PicController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetImage(int id)
+        {
+            var webRoot = _env.WebRootPath;
+            var path = Path.Combine(webRoot + "/pics/", "shoes-" + id + ".png");
+            var buffer = System.IO.File.ReadAllBytes(path);
+            return File(buffer, "image/png");
+        }
+    }
+}
